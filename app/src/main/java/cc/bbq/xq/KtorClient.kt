b460961b8449ab747bdc1057fa8da017007128bb
Voltula <3196570237@qq.com>
2025-10-18 14:15:51 +0800

    更新

--- app/src/main/java/cc/bbq/xq/KtorClient.kt
+++ app/src/main/java/cc/bbq/xq/KtorClient.kt
@@ -7,6 +7,7 @@ import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsText
 import io.ktor.client.request.forms.*
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.*
