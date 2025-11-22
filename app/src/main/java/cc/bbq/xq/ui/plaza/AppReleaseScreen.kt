9355a48041eecdaa26618ca72c60fba0908dcff6
Voltula <3196570237@qq.com>
2025-11-23 02:15:14 +0800

    更新

--- app/src/main/java/cc/bbq/xq/ui/plaza/AppReleaseScreen.kt
+++ app/src/main/java/cc/bbq/xq/ui/plaza/AppReleaseScreen.kt
@@ -2,7 +2,6 @@
 // 本程序是自由软件：你可以根据自由软件基金会发布的 GNU 通用公共许可证第3版
 //（或任意更新的版本）的条款重新分发和/或修改它。
 // 本程序是基于希望它有用而分发的，但没有任何担保；甚至没有适销性或特定用途适用性的隐含担保。
-// 有关更多细节，请参阅 GNU 通用公共许可证。
 //
 // 你应该已经收到了一份 GNU 通用公共许可证的副本
 // 如果没有，请查阅 <http://www.gnu.org/licenses/>.
@@ -354,7 +353,7 @@ private fun ApkUploadServiceDropdown(viewModel: AppReleaseViewModel) {
                 label = { Text("选择上传服务") },
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
                 modifier = Modifier
-                    .menuAnchor(type = ExposedDropdownMenuAnchorType.Surface) // fixed: use menuAnchor with type
+                    //fixed: remove menuAnchor
                     .fillMaxWidth()
             )
             ExposedDropdownMenu(
@@ -417,7 +416,7 @@ private fun CategoryDropdown(viewModel: AppReleaseViewModel) {
                 label = { Text("选择一个分类") },
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
                 modifier = Modifier
-                    .menuAnchor(type = ExposedDropdownMenuAnchorType.Surface)  // fixed: use menuAnchor with type
+                   //fixed: remove menuAnchor
                     .fillMaxWidth()
             )
             ExposedDropdownMenu(
