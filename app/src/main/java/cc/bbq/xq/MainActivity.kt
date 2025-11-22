99205bc37e23f2eb8e4d36a50a0262f360d78bc0
Voltula <3196570237@qq.com>
2025-11-23 01:29:35 +0800

    更新

--- app/src/main/java/cc/bbq/xq/MainActivity.kt
+++ app/src/main/java/cc/bbq/xq/MainActivity.kt
@@ -100,8 +100,8 @@ class MainActivity : ComponentActivity() {
 
         lifecycleScope.launch {
             delay(10000)
-            val context = this@MainActivity
-            val userCredentialsFlow = AuthManager.getCredentials(context)
+            val contextForHeartbeat = this@MainActivity // fixed: rename context to avoid shadowing
+            val userCredentialsFlow = AuthManager.getCredentials(contextForHeartbeat)
             val userCredentials = userCredentialsFlow.first()
             if (userCredentials != null) {
                 startHeartbeatService(this@MainActivity, userCredentials.token)
@@ -411,7 +411,7 @@ fun MainComposeApp(snackbarHostState: SnackbarHostState) {
         val userCredentialsFlow = AuthManager.getCredentials(context)
         val userCredentials = userCredentialsFlow.first()
         // 检查 userCredentials 是否存在，且 username 和 password 都不为空
-        isLoggedIn.value = userCredentials!!.userId != 0L
+        isLoggedIn.value = userCredentials?.userId != 0L // fixed: remove unnecessary non-null assertion
 
         // 只有在用户已登录的情况下才尝试自动登录
         if (isLoggedIn.value) {
